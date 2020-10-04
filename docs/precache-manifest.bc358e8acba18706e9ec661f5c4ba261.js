4a4598d87ec0afb394987ad49c6cb07575f741c8
Nicolas Agreda <1930.nicolas@gmail.com>
2020-10-03 22:12:48 -0500

    docs agregado

---
+++ docs/precache-manifest.bc358e8acba18706e9ec661f5c4ba261.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "40b954902fe8c6294703f47a6137478b",
+    "url": "/index.html"
+  },
+  {
+    "revision": "ea383d86de1ed222c049",
+    "url": "/static/css/main.d9c14ad6.chunk.css"
+  },
+  {
+    "revision": "e963e9842dc7094e0746",
+    "url": "/static/js/2.cce1445f.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.cce1445f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ea383d86de1ed222c049",
+    "url": "/static/js/main.f73475b6.chunk.js"
+  },
+  {
+    "revision": "daee87f482498cdf724c",
+    "url": "/static/js/runtime-main.29242b1d.js"
+  }
+]);
